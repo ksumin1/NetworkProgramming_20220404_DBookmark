87be0041c70e4e357a94dbc5698fbc4f85a54b2b
user <w2005@e-mirim.hs.kr>
2022-04-08 14:24:08 +0900

    admin Bookmark

--- bookmark/models.py
+++ bookmark/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Bookmark(models.Model):
     name = models.CharField(max_length=10)
     url = models.URLField()
+
+    def __str__(self):
+        return f'{self.name} : {self.url}'
